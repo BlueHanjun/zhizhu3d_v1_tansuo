d4e750a9b9d4607b11fdc8ee10ff4b23797eef08
[dyad] <git@dyad.sh>
2025-08-06 15:18:05 +0800

    [dyad] 更新文档中心链接 - wrote 1 file(s)

--- src/pages/DocsPage.tsx
+++ src/pages/DocsPage.tsx
@@ -3,7 +3,7 @@ const DocsPage = () => {
     <div className="container mx-auto flex h-full flex-col items-center justify-center p-4 md:p-6">
       <div className="w-full h-[calc(100vh-12rem)] bg-zinc-900 flex items-center justify-center rounded-lg">
         <iframe
-          src="https://react.dev" // 这是一个占位符网址，您可以替换成您的文档地址
+          src="https://fxnhbt6c1n.feishu.cn/wiki/space/7534718019357032452?ccm_open_type=lark_wiki_spaceLink&open_tab_from=wiki_home" // 这是一个占位符网址，您可以替换成您的文档地址
           title="Documentation"
           className="h-full w-full border-0 rounded-lg"
         />
